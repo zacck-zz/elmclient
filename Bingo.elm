af5bdcd72217b9dc097d92ac7b5791d0df20948b
Zacck Osiemo <coderv63@gmail.com>
2017-12-11 07:24:41 +0200

    add type annotations to the functions in the app

--- Bingo.elm
+++ Bingo.elm
@@ -6,9 +6,11 @@ import Html.Attributes exposing (..)
 --main = 
 --    Html.text (String.repeat 3 (String.toUpper "Another New Elm Application! "))
 
+playerInfo : String -> String -> String
 playerInfo name gameNumber =
     name ++ " Game number " ++ gameNumber
 
+viewPlayer : String -> String -> Html msg 
 viewPlayer name gameNumber =
    let
     playerInfoText =
@@ -19,16 +21,19 @@ viewPlayer name gameNumber =
     h2 [ id "info", class "classy"]
     [ playerInfoText ]
 
+viewHeader : String -> Html msg
 viewHeader title =
    header [] 
    [ h1 [] [ text title ] ]
 
+viewFooter : Html msg
 viewFooter =
    footer [] 
    [a [href "http://github.com/zacck"]
       [text "Built by Zacck"]
    ]
 
+view : Html msg
 view =
     div [ class "content" ]
     [ viewHeader "MOFO Bingo",
@@ -36,6 +41,6 @@ view =
       viewFooter
     ]
 
-
+main : Html msg
 main =
    view
